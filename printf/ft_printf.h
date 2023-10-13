06cbf94dc91da80e26bf5d3f95b69395da4fc3b7
Nika <veimnikki@gmail.com>
2023-10-13 09:13:20 +0600

    header and makefile were added

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -2,11 +2,15 @@
 #define FT_PRINTF_H
 
 #include <stdarg.h>
+#include <stdint.h>
 
 int ft_printf(const char *formats, ...);
 int	ft_printstr(char *str);
 int ft_print_ptr(unsigned long long ptr);
 int ft_print_unsigned(unsigned int n);
 int ft_print_hex(unsigned int n, const char *format);
+int	ft_printnbr(int n);
+int	ft_printpercent(void);
+int ft_formats(va_list args, const char format);
 
 #endif
